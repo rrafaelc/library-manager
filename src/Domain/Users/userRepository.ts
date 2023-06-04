9d0a16d9060b4cca968bb649af5f10f730857af1
Rafael Costa <rafaelcosta8020@gmail.com>
2023-06-04 13:37:52 -0300

    add some methods to userRepository

--- src/Domain/Users/userRepository.ts
+++ src/Domain/Users/userRepository.ts
@@ -1,12 +1,61 @@
 import { CreateUserDto } from '@Application/Users/dtos/createUserDto';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User } from '@prisma/client';
 
 export class UserRepository {
   private prisma = new PrismaClient();
 
+  async find() {
+    return this.prisma.user.findMany();
+  }
+
+  async findById(id: string) {
+    return this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+  }
+
+  async findByCpfCnpj(cpf_cnpj: string) {
+    return this.prisma.user.findFirst({
+      where: {
+        cpf_cnpj,
+      },
+    });
+  }
+
   async create(user: CreateUserDto) {
     return this.prisma.user.create({
       data: user,
+    });
+  }
+
+  async update(user: User) {
+    return this.prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: user,
+    });
+  }
+
+  async softDelete(user: User, isActive: boolean) {
+    //
+  }
+
+  async delete(user: User) {
+    return this.prisma.user.delete({
+      where: {
+        id: user.id,
+      },
     });
   }
 }
